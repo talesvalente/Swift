dc2f7763aa93ab8050993fdd82573adeb2b82ae4
Tales Valente <talesvalente@gmail.com>
2021-09-20 19:00:19 -0300

    Create Arraymode.swift (beta / needs review)
    +Known bugs: Array out of Range at end of file.
    +Some variables are set wrong.
    +Need review

---
+++ arraymode.swift
@@ -0,0 +1,81 @@
+import Foundation
+                                                                                                                                                      
+/*                                                                                                                                                       
+*        CCCCCCCCCCCCC        66666666                    BBBBBBBBBBBBBBBBB               AAA               NNNNNNNN        NNNNNNNNKKKKKKKKK    KKKKKKK
+*     CCC::::::::::::C       6::::::6                     B::::::::::::::::B             A:::A              N:::::::N       N::::::NK:::::::K    K:::::K
+*   CC:::::::::::::::C      6::::::6                      B::::::BBBBBB:::::B           A:::::A             N::::::::N      N::::::NK:::::::K    K:::::K
+*  C:::::CCCCCCCC::::C     6::::::6                       BB:::::B     B:::::B         A:::::::A            N:::::::::N     N::::::NK:::::::K   K::::::K
+* C:::::C       CCCCCC    6::::::6                          B::::B     B:::::B        A:::::::::A           N::::::::::N    N::::::NKK::::::K  K:::::KKK
+*C:::::C                 6::::::6                           B::::B     B:::::B       A:::::A:::::A          N:::::::::::N   N::::::N  K:::::K K:::::K   
+*C:::::C                6::::::6                            B::::BBBBBB:::::B       A:::::A A:::::A         N:::::::N::::N  N::::::N  K::::::K:::::K    
+*C:::::C               6::::::::66666     ---------------   B:::::::::::::BB       A:::::A   A:::::A        N::::::N N::::N N::::::N  K:::::::::::K     
+*C:::::C              6::::::::::::::66   -:::::::::::::-   B::::BBBBBB:::::B     A:::::A     A:::::A       N::::::N  N::::N:::::::N  K:::::::::::K     
+*C:::::C              6::::::66666:::::6  ---------------   B::::B     B:::::B   A:::::AAAAAAAAA:::::A      N::::::N   N:::::::::::N  K::::::K:::::K    
+*C:::::C              6:::::6     6:::::6                   B::::B     B:::::B  A:::::::::::::::::::::A     N::::::N    N::::::::::N  K:::::K K:::::K   
+* C:::::C       CCCCCC6:::::6     6:::::6                   B::::B     B:::::B A:::::AAAAAAAAAAAAA:::::A    N::::::N     N:::::::::NKK::::::K  K:::::KKK
+*  C:::::CCCCCCCC::::C6::::::66666::::::6                 BB:::::BBBBBB::::::BA:::::A             A:::::A   N::::::N      N::::::::NK:::::::K   K::::::K
+*   CC:::::::::::::::C 66:::::::::::::66                  B:::::::::::::::::BA:::::A               A:::::A  N::::::N       N:::::::NK:::::::K    K:::::K
+*     CCC::::::::::::C   66:::::::::66                    B::::::::::::::::BA:::::A                 A:::::A N::::::N        N::::::NK:::::::K    K:::::K
+*        CCCCCCCCCCCCC     666666666                      BBBBBBBBBBBBBBBBBAAAAAAA                   AAAAAAANNNNNNNN         NNNNNNNKKKKKKKKK    KKKKKKK
+*
+*PROGRAMA DE TREINAMENTO P/ DESENVOLVIMENTO iOS
+*LINGUAGEM DE ALTO NÍVEL SWIFT - (MODIFICADORES DE ARRAY)
+*By: Tales Valente
+*/
+
+//COLEÇÃO DE DADOS ACESSADOS POR INDICE
+
+//let userNames: [String] = ["Tales", "Valente", "C6BANK"]
+let userNames = ["Tales", "Valente", "C6BANK"]
+print(userNames)
+
+var userName: [String] = []
+userName.append("Tales")
+print(userName)
+userName += ["Valente","C6BANK"]
+print(userName)
+print(userName.count)
+
+//SLICE
+let name = userName[0]
+print(name)
+
+let firstThree = userNames[1...2]
+print(firstThree[1])
+
+let firstThree2 = Array(userNames[1...2])
+print(firstThree2[0])
+
+// Remover todos os elementos
+userName.removeAll()
+
+// Checha se é vazio
+print(userNames.isEmpty)
+
+// Verificar Tamanho
+print(userNames.count)
+
+// Verificar se tem algum elemento especifico
+print(userNames.contains("Tales"))
+
+// Acessar o primeiro elemento (?)
+if let first = userNames.first { 
+  print(first)
+}
+
+//Inserir no Indice
+userName.insert("Inserido Aqui", at: 0)
+print (userName)
+
+//Remover no Indice
+userName.remove(at: 0)
+print(userName)
+
+userName.insert("Inserido2", at: 0)
+print (userName)
+
+let removedLast = userName.removeLast()
+print (removedLast)
+
+userName[0...1] = ["Novo1","Novo2","Novo3"]
+print(userName)
